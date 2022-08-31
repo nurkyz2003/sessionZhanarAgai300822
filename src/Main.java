2647340296c5cf68150a5b747e593e7147f22133
nurkyz2003 <110599334+nurkyz2003@users.noreply.github.com>
2022-08-31 19:55:47 +0600

    hi

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,14 @@ public class Main {
             employees[i].setFirstname(writeName(i,"first name "));
             employees[i].setLastname(writeName(i,"last name  "));
             employees[i].setBirthYear(writeYear(i,"birth year "));
-            employees[i].setStartWorkYear(writeWorkYear(i,"work year "));
+            employees[i].setStartWorkYear(writeWorkYear(i,employees[i].getBirthYear(),"work year "));
         }
         for (Employee i : employees) {
-            System.out.println(i.getFirstname());
-            System.out.println(i.getLastname());
-            System.out.println(i.getBirthYear());
-            System.out.println(i.getStartWorkYear());
+            System.out.println("-------------------------------------------");
+            System.out.println("Name: " + i.getFirstname());
+            System.out.println("Surname: " + i.getLastname());
+            System.out.println("Birth Year: " + i.getBirthYear());
+            System.out.println("Begin Work Year : " + i.getStartWorkYear());
         }
     }
     public static String writeName(int i,String word){
@@ -64,20 +65,25 @@ public class Main {
             }
         }
     }
-    public static int writeWorkYear(int i, String word) {
-        while (true) {
-            try {
-                System.out.printf("Enter %d'th employe's %s", i, word);
-                int year = scanner.nextInt();
-                if (year > 1950) {
-                    scanner.nextLine();
-                    return year;
-                }
-                throw new Exception();
-            } catch (Exception e) {
-                scanner.nextLine();
-                System.out.printf("Error %d'th employe's %s", i, word);
+    public static int writeWorkYear(int i, int birthYear, String word){
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter %d'th employee's %s ", i, word);
+        int workYear = scanner.nextInt();
+        try {
+            if (checkWorkYear(workYear, birthYear))
+                throw new Exception("Error workYear");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            while (checkWorkYear(workYear, birthYear)){
+                System.out.printf("Enter %d'th employee's %s again: ", i, word);
+                workYear = scanner.nextInt();
             }
         }
+        return workYear;
+    }
+
+    public static boolean checkWorkYear(int workYear, int birthYear){
+        return (workYear - birthYear) < 18;
     }
 }
